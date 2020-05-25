4c82eed257f5c42dc735c63038cca1c80346e5da
Captain-dikal <33635085+Captain-dikal@users.noreply.github.com>
2020-05-25 08:36:46 +0300

    Update chat.js
    edit   on

--- lab6/task3/chat.js
+++ lab6/task3/chat.js
@@ -2,15 +2,12 @@ function ChatUI() {
   this.addMessage = function(messageText, messageSenderType) {
     var chat = document.getElementById('chat-wrap');
     var text = messageText;
-
     if (text !== '') {
       var newMessageContainer = document.createElement('div');
       var newMessageAuthor = document.createElement('h2');
       var newMessageText = document.createElement('p');
-
       newMessageAuthor.classList.add('chat-wrap-element_title');
       newMessageText.innerText = text;
-
       if (messageSenderType === 'client') {
         newMessageAuthor.innerText = 'You';
         newMessageText.classList.add('chat-wrap-element_text');
@@ -22,16 +19,15 @@ function ChatUI() {
         newMessageContainer.classList.add('chat-wrap-element_out');
         newMessageContainer.classList.remove('chat-wrap-element_in');
       }
-
       newMessageContainer.append(newMessageAuthor, newMessageText);
       chat.append(newMessageContainer);
     }else{
-		alert('Введите текст сообщения!');
-	}
+      alert('Введите текст сообщения!');
+    }
   };
 }
 window.onload = function() {
-  chat = new ChatUI();
+  var chat = new ChatUI();
   var sendButton = document.getElementById('chat-send-wrap_btn');
   sendButton.onclick = function() {
     var text = document.getElementById('chat-send-wrap_textarea').value;
